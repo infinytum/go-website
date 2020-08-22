06c06c897385b5f17bd46cdb22defdb5003c8d25
michael.teuscher@protonmail.com <nila@Michaels-MBP.localdomain>
2020-08-22 19:15:51 +0200

    fuck

--- pkg/context/language.go
+++ pkg/context/language.go
@@ -1,10 +1,16 @@
 package context
 
-import "github.com/infinytum/go-website/pkg/language"
+import (
+	"github.com/infinytum/go-website/internal/config"
+	"github.com/infinytum/go-website/pkg/language"
+)
 
 func (ctx *Application) LanguageService() *language.Language {
 	if ctx.language == nil {
-		ctx.language = &language.Language{}
+		ctx.language = &language.Language{
+			Config: ctx.config,
+			Cache:  make(map[string]config.Config),
+		}
 	}
 	return ctx.language
 }
